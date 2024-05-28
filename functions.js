c3d7951e71258e58268fa113ffb8fb7717d7d726
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-05-28 23:23:57 +0800

    add button functionality

--- functions.js
+++ functions.js
@@ -7,9 +7,28 @@ function loadHTML(url, elementId) {
             return response.text();
         })
         .then(data => {
-            document.getElementById(elementId).innerHTML = data;
+          document.getElementById(elementId).innerHTML = data;
         })
         .catch(error => {
             console.error('Error fetching the HTML:', error);
         });
+}
+
+function openEmail() {
+  var emailAddress = 'jtlagumbay@up.edu.ph';
+  var subject = 'Let\'s Connect';
+
+  var mailtoLink = 'mailto:' + encodeURIComponent(emailAddress);
+  if (subject) {
+    mailtoLink += '?subject=' + encodeURIComponent(subject);
+  }
+  console.log(mailtoLink)
+  window.open(mailtoLink);
+}
+
+function openLinkedIn() {
+  window.open(
+    "https://www.linkedin.com/in/lagumbay-jhoanna-rica-37a45520a/",
+    "_blank"
+  )
 }
