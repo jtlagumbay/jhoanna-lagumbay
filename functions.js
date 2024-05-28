2c0035e7ac6c93371f95a6b7beabdf0ee37cad47
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-05-29 01:39:08 +0800

    add project section

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
-function loadHTML(url, elementId) {
-    fetch(url)
+async function loadHTML(url, elementId, callback) {
+  console.log(url, elementId)
+    await fetch(url)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -8,6 +9,9 @@ function loadHTML(url, elementId) {
         })
         .then(data => {
           document.getElementById(elementId).innerHTML = data;
+          if (callback) {
+              callback();
+          }
         })
         .catch(error => {
             console.error('Error fetching the HTML:', error);
@@ -31,4 +35,61 @@ function openLinkedIn() {
     "https://www.linkedin.com/in/lagumbay-jhoanna-rica-37a45520a/",
     "_blank"
   )
+}
+
+function openGithub() {
+  window.open(
+    "https://github.com/jtlagumbay/",
+    "_blank"
+  )
+}
+
+async function populateProjects() {
+  var project_card_container = document.getElementById("PROJECT-CARD-CONTAINER")
+  if(project_card_container){
+    const projects = await fetch('projects.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        return data.projects
+      })
+      .catch(error => {
+        console.error('Error fetching the JSON:', error);
+      });
+    
+    const project_child = await fetch('project-card.html')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        return data
+      })
+      .catch(error => {
+        console.error('Error fetching the JSON:', error);
+      });
+    const parser = new DOMParser();
+    const project_child_html = parser.parseFromString(project_child, 'text/html')
+    projects.map(project => {
+      var new_child = project_child_html
+      console.log(project)
+      new_child.getElementById("PROJECT-CARD-TITLE").innerText = project.title
+      console.log(new_child)
+      project_card_container.appendChild(new_child.documentElement)
+    })
+  }
+}
+
+let images = ["./images/guia-1.png", "./images/guia-2.png", "./images/guia-3.png", "./images/guia-4.png"]
+let index = 0;
+function changeThumbnail() {
+  const imgElement = document.querySelector('#PROJECT-THUMBNAIL');
+  imgElement.src = images[index];
+   index > 1 ? index = 0 : index++;
 }
